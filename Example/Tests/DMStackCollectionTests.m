8470408b18bd0a5cee9f2afea4716938c773d02c
Umang Shah <umang.shah@okta.com>
2015-04-15 00:15:22 -0700

    2

--- Example/Tests/DMStackCollectionTests.m
+++ Example/Tests/DMStackCollectionTests.m
@@ -6,45 +6,54 @@
 //  Copyright (c) 2015 Chandra Shirashyad. All rights reserved.
 //
 
-Story: Stack Collection
+SPEC_BEGIN(DMStackCollectionSpecs)
 
+describe(@"with a stack collection", ^{
+    
+    context(@"when creating a new collection", ^{
+        
+        pending(@"the collection should exist", ^{
+            
+        });
+    });
+    
+    context(@"pushing an object onto the stack", ^{
+        
+        pending(@"the stack count should be one", ^{
+            
+        });
+        
+    });
+    
+    context(@"popping an object off of the stack", ^{
+        
+        pending(@"the popped object should be the same as pushed", ^{
+            
+        });
+        
+    });
+    
+    context(@"peeking at the topmost object on the stack", ^{
+        
+        pending(@"the popped object should be the same as the last pushed", ^{
+            
+        });
+        
+        pending(@"the stacks object count should be unaffected by peeking", ^{
+            
+        });
+        
+        
+    });
+    
+    context(@"the stack should contain at least 10 concurrent objects", ^{
+        
+        pending(@"add objects, compare after popping and check stack count", ^{
+            
+        });
+        
+    });
+    
+});
 
-In order to represent a collection of objects that operate as a stack
-as a developer
-I want to push objects onto the stack
-and pop objects off the stack
-and peek at the topmost object on the stack without affecting the stack
-and the stack must handle at least 10 concurrent objects
-
-
-Scenario 1: Create a stack collection
-Given a class representing a stack collection
-When I create a new stack
-Then the stack should exist
-
-
-Scenario 2: Push an object onto the stack
-Given a newly created stack
-When I push an object
-Then the stack should contain one object
-
-
-Scenario 3: Pop an object off the stack
-Given  a newly created stack
-And I push an object onto the stack
-When I pop the object off the stack
-Then the object should match the object I pushed onto the stack
-
-
-Scenario 4: Peek at the topmost object on the stack
-Given a stack with one object
-When I peek at the topmost object on the stack
-Then the stack should contain one object
-
-
-Scenario 5: The stack should handle at least 10 concurrent objects
-Given a newly created stack
-When I push 12 objects onto the stack
-and I pop 12 objects off the stack
-Then the objects should be identical to those pushed in last in, first out order
-And the stack should no longer contain objects
+SPEC_END
